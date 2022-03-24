99de7d615edfa2232ce9aaea93842afd12ded221
Claudia <clau.anaya.c18@gmail.com>
2022-03-23 18:14:02 -0600

    esqueleto

---
+++ src/main/java/com/examen/primera/unidad/examenPrimeraUnidad/utils/CustomResponse.java
@@ -0,0 +1,49 @@
+package com.examen.primera.unidad.examenPrimeraUnidad.utils;
+
+import java.util.LinkedList;
+
+
+
+public class CustomResponse {
+	 private Integer httpCode;
+	    private Object data;
+	    private String mesage;
+	    
+	    public CustomResponse(){
+	        this.httpCode = 200;
+	        data = new LinkedList();
+	        this.mesage = "Ok";
+	    }
+	    
+	    public void updateArticulo(int idArticulo, Object articulo){ //ArticuloModel
+	        data = articulo;
+	    }
+	    
+	    public void deleteArticulo(int idArticulo){
+	        
+	    }
+
+	    public Integer getHttpCode() {
+	        return httpCode;
+	    }
+
+	    public void setHttpCode(Integer httpCode) {
+	        this.httpCode = httpCode;
+	    }
+
+	    public Object getData() {
+	        return data;
+	    }
+
+	    public void setData(Object data) {
+	        this.data = data;
+	    }
+
+	    public String getMesage() {
+	        return mesage;
+	    }
+
+	    public void setMesage(String mesage) {
+	        this.mesage = mesage;
+	    }
+}
